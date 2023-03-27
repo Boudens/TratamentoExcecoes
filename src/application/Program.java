407822826585d2bf6e71c91676c57e7cd56978d7
Caio Boudens <caioboudens00@gmail.com>
2023-03-27 16:40:24 -0300

    primeira solução

---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		System.out.print("Room: ");
+		int roomNumber = sc.nextInt();
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		LocalDate checkIn = LocalDate.parse(sc.next(), fmt1);
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		LocalDate checkOut = LocalDate.parse(sc.next(), fmt1);
+
+		if (!checkOut.isAfter(checkIn)) {
+			System.out.println("Error in reservation: check-out date must be after check-in date");
+		} else {
+			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+			System.out.println(reservation);
+
+			System.out.println("Enter data to update reservation: ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			LocalDate checkIn1 = LocalDate.parse(sc.next(), fmt1);
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			LocalDate checkOut1 = LocalDate.parse(sc.next(), fmt1);
+			if (checkOut1.isBefore(checkOut) || checkIn1.isBefore(checkIn)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates.");
+
+			} else {
+				if (!checkOut1.isAfter(checkIn1)) {
+					System.out.println("Error in reservation: check-out date must be after check-in date.");
+				} else {
+					reservation.updateDates(checkIn1, checkOut1);
+					System.out.println(reservation);
+				}
+			}
+		}
+
+		sc.close();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author caiob
+ *
+ */
+module excecoes {
+}
